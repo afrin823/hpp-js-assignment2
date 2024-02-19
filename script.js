9e364bf3422d47652a42b3b7a0730fff5574c41d
Afrin <154747276+afrin823@users.noreply.github.com>
2024-02-19 14:42:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//-------01 arry
+
+let laptopBrand = ['waltop', 'hp', 'tusiba', 'Applw'];
+
+console.log(laptopBrand);
+//------- object
+let mobile = {
+    name: 'iphone',
+    modle: 56,
+    price: 150000,
+}
+console.log(mobile);
+
+///---- push
+let allSutdent = ['afrin', 'nahar', 'maysa', 'mithila'];
+
+laptop = laptopBrand.push('Asha') ;
+console.log(laptop);
+/// ------- pop
+let flowers = ['rose', 'beli', 'joba'];
+
+flowers.pop();
+console.log(flowers);
+//-----------indexOf
+let food = ['sabikun', 'nahar', 'afrin'];
+
+food.indexOf('mita');
+
+console.log(food);
+
+let fullName = 'sabikun nahar afrin';
+
+console.log(fullName.length);
+
+
+let myAge = 18;
+
+if(myAge >= 18){
+    console.log('i will create a bank account')
+}else{
+    console.log('i am not create a bank account')
+}
+
+
+
